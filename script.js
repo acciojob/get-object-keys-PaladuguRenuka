f42fc366db12a3379c7f4b7d0ef66a54780afbc5
PaladuguRenuka <166904796+PaladuguRenuka@users.noreply.github.com>
2024-08-22 11:40:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student = {
+	name : 'Renuka'
+}
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+console.log(student.getKeys());
